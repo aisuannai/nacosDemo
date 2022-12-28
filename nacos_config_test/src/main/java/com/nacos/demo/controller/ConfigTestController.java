1bcdcf7f5f4250b6acdb3a2efe1304000b3c6bb1
zhaiwy <1433964146@qq.com>
2022-12-28 19:29:43 +0800

    nacos配置中心demo

---
+++ nacos_config_test/src/main/java/com/nacos/demo/controller/ConfigTestController.java
@@ -0,0 +1,22 @@
+package com.nacos.demo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/config")
+@RefreshScope
+public class ConfigTestController {
+
+    @Value("${user.localName:}")
+    private String name;
+    @Value("${user.age:}")
+    private String age;
+    @GetMapping("/test")
+    public String getConfig(){
+        return name + ":" + age;
+    }
+}
